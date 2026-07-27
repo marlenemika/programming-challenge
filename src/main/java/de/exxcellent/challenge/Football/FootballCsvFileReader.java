0f63b21a0fcf22c387722c331d425cbabae6d52c
Marlene Mika <marlene.mika@uni-ulm.de>
2026-07-27 18:12:33 +0200

    feat(football): add football csv file reader

---
+++ src/main/java/de/exxcellent/challenge/Football/FootballCsvFileReader.java
@@ -0,0 +1,47 @@
+package de.exxcellent.challenge.Football;
+
+import de.exxcellent.challenge.CsvFileReader.CsvFileReader;
+
+public class FootballCsvFileReader extends CsvFileReader<FootballData> {
+
+    public FootballCsvFileReader(String filePath) {
+        super(filePath);
+    }
+
+    @Override
+    protected FootballData parseLineToDataObject(String line) {
+        String[] lineContent = line.split(",|\\n");
+        return new FootballData(lineContent);
+    }
+
+    /**
+     * Checks if the line contains the required column names to continue
+     * 
+     * @param line the line to be checked
+     * @return true if the line contains all column names required, else false
+     */
+    protected boolean isValidHeader(String line) {
+        boolean containsNeededArguments = true;
+        String[] minArguments = new String[]{"Team", "Goals", "Goals Allowed"};
+        for (String argument : minArguments) {
+            containsNeededArguments = containsNeededArguments && line.contains(argument);
+        }
+        return containsNeededArguments;
+    }
+
+    /**
+     * Checks if the line has exactly 8 values
+     * The first entry is the team name (including letter characters, underscores and spaces)
+     * The remaining 7 entries are all integers
+     * The first 7 values are each followed by a comma to seperate them
+     * The last value is not required to be followed by a comma
+     * 
+     * @param line the line to be checked
+     * @return true if the line contains 8 values following that pattern
+     */
+    @Override
+    protected boolean isValidLine(String line) {
+        return line.matches("[A-Za-z_ ]+(?:,\\d+){7}");
+    }
+    
+}
---
+++ src/test/java/de/exxcellent/challenge/Football/FootballCsvFileReaderTest.java
@@ -0,0 +1,73 @@
+package de.exxcellent.challenge.Football;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+import de.exxcellent.challenge.CsvFileReader.InvalidLineException;
+
+public class FootballCsvFileReaderTest {
+
+    final String SHARED_PATH = "src/test/java/de/exxcellent/challenge/resources/";
+
+    final String TEST_FILE_PATH = SHARED_PATH + "testFileFootball.csv";
+    final String EMPTY_FILE_PATH = SHARED_PATH + "emptyFile.csv";
+    final String NON_EXISTING_FILE_PATH = SHARED_PATH + "faultyFile.csv";
+    final String INVALID_TEST_FILE_PATH = SHARED_PATH + "invalidTestFileFootball.csv";
+    
+    @Test
+    void readsFileContentCorrectly() {
+        FootballCsvFileReader reader = new FootballCsvFileReader(TEST_FILE_PATH);
+
+        ArrayList<FootballData> testFileContent = new ArrayList<>();
+        testFileContent.add(new FootballData("Manchester", 20, 10));
+        testFileContent.add(new FootballData("PSG__ ", 5, 42));
+
+        assertEquals(testFileContent, reader.parseFile());
+    }
+
+    @Test
+    void readsEmptyFile() {
+        FootballCsvFileReader reader = new FootballCsvFileReader(EMPTY_FILE_PATH);
+
+        assertTrue(reader.parseFile().isEmpty());
+    }
+
+    @Test
+    void returnsEmptyListForNonExistingFile() {
+        FootballCsvFileReader reader = new FootballCsvFileReader(NON_EXISTING_FILE_PATH);
+
+        assertTrue(reader.parseFile().isEmpty());
+    }
+
+    @Test
+    void ignoresHeaderLine() {
+        FootballCsvFileReader reader = new FootballCsvFileReader(TEST_FILE_PATH);
+
+        ArrayList<FootballData> result = reader.parseFile();
+
+        assertEquals(2, result.size());
+    }
+
+    @Test
+    void parseAllEntries() {
+        FootballCsvFileReader reader = new FootballCsvFileReader(TEST_FILE_PATH);
+
+        ArrayList<FootballData> result = reader.parseFile();
+
+        assertEquals(2, result.size());
+    }
+
+    @Test
+    void throwsExceptionForInvalidLine() {
+        FootballCsvFileReader reader = new FootballCsvFileReader(INVALID_TEST_FILE_PATH);
+
+        InvalidLineException exception = assertThrows(
+            InvalidLineException.class, () -> reader.parseFile()
+        );
+
+        assertEquals("Invalid line: Manchester,20,10", exception.getMessage());
+    }
+}
