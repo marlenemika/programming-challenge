f311d51d0706852740d54fc819e9328b64874901
Marlene Mika <marlene.mika@uni-ulm.de>
2026-07-27 03:54:32 +0200

    refactor(app): remove unused import

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,7 +1,5 @@
 package de.exxcellent.challenge;
 
-import java.util.List;
-
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
